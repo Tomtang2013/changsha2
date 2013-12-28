11d507efa3f88ae75da5f2e2a7714369e3c6b37f
Tom tang <tom.tang@perficientgdc.com.cn>
2013-12-28 19:51:23 +0800

    add about and work

---
+++ sites/all/themes/changshanews/templates/about/team.tpl.php
@@ -0,0 +1,146 @@
+<?php
+global $base_path;
+$about_company_path = $base_path . "about/company";
+$about_team_path = $base_path . "about/team";
+$theme_path = $base_path . drupal_get_path('theme', 'changshanews');
+?>
+
+<script type="text/javascript">
+    jQuery(function(){
+        jQuery( ".active" ).find('a').css('color','black');
+        jQuery( ".unactive" ).find('a').css('color','#3c3b39');
+    });
+</script>
+
+
+<div id="work_about" class="work_about ">
+    <ul class="ul_left">
+        <li class="unactive"><a href="<?php print $about_company_path; ?>">关于公司</a></li>
+        <li class="active"><a href="<?php print $about_team_path; ?>">关于团队</a></li>
+    </ul>
+
+    <div class="div_right">
+        <span class="">A BRAND / A SOUND</span>
+        <span>一个品牌一个声音</span>
+    </div>
+
+    <div class="work_about_team_main" >
+        <div class="team_member_list">
+            <div><p>顾问专家</p></div>
+            <div class="team_member_imgs">
+                <div class="team_member_img">
+                    <div class="member_img "><a href=""></div>
+                    <div class="member_name">龙博</div>
+                </div>
+
+                <div class="team_member_img">
+                    <div class="member_img "><a href=""></div>
+                    <div class="member_name">龙博</div>
+                </div>
+
+                <div class="team_member_img">
+                    <div class="member_img "><a href=""></div>
+                    <div class="member_name">龙博</div>
+                </div>
+
+                <div class="team_member_imglast">
+                    <div class="member_img "><a href=""></div>
+                    <div class="member_name">龙博</div>
+                </div>
+
+                <div class="team_member_img">
+                    <div class="member_img "><a href=""></div>
+                    <div class="member_name">龙博</div>
+                </div>
+
+                <div class="team_member_img">
+                    <div class="member_img "><a href=""></div>
+                    <div class="member_name">龙博</div>
+                </div>
+
+                <div class="team_member_img">
+                    <div class="member_img "><a href=""></div>
+                    <div class="member_name">龙博</div>
+                </div>
+
+                <div class="team_member_imglast">
+                    <div class="member_img "><a href=""></div>
+                    <div class="member_name">龙博</div>
+                </div>
+
+            </div>
+        </div>
+        <div style="clear:both;">
+        <div class="team_member_list">
+            <div><p>专家顾问</p></div>
+            <div class="team_member_imgs">
+                <div class="team_member_img">
+                    <div class="member_img img1"></div>
+                    <div class="member_name">施晓玲</div>
+                </div>
+
+                <div class="team_member_img">
+                    <div class="member_img img2"></div>
+                    <div class="member_name">黄昌勇</div>
+                </div>
+
+                <div class="team_member_img">
+                    <div class="member_img "><a href=""></div>
+                    <div class="member_name">刘哲</div>
+                </div>
+
+                <div class="team_member_imglast">
+                    <div class="member_img img3"></div>
+                    <div class="member_name">吴威</div>
+                </div>
+
+                <div class="team_member_img">
+                    <div class="member_img img4"></div>
+                    <div class="member_name">方敏</div>
+                </div>
+
+                <div class="team_member_img">
+                    <div class="member_img img5"></div>
+                    <div class="member_name">张萌</div>
+                </div>
+
+                <div class="team_member_img">
+                    <div class="member_img img6"></div>
+                    <div class="member_name">杨靖舟</div>
+                </div>
+
+                <div class="team_member_imglast">
+                    <div class="member_img img7"></div>
+                    <div class="member_name">舒靖期</div>
+                </div>
+
+                <div class="team_member_img">
+                    <div class="member_img img8"></div>
+                    <div class="member_name">马藤兹</div>
+                </div>
+
+                <div class="team_member_img">
+                    <div class="member_img img9"></div>
+                    <div class="member_name">曾扬</div>
+                </div>
+
+                <div class="team_member_img">
+                    <div class="member_img img0"></div>
+                    <div class="member_name">胡彦</div>
+                </div>
+
+                <div class="team_member_imglast">
+                    <div class="member_img img0"></div>
+                    <div class="member_name">童亦</div>
+                </div>
+
+                <div class="team_member_img">
+                    <div class="member_img img0"></div>
+                    <div class="member_name">王子诺</div>
+                </div>
+            </div>
+        </div>
+        <div style="clear:both;"></div>
+    </div>
+</div>
+
---
+++ sites/all/themes/changshanews/templates/work/enviro.tpl.php
@@ -0,0 +1,45 @@
+<?php
+global $base_path;
+$theme_path = $base_path . drupal_get_path('theme', 'changshanews');
+?>
+
+<script type="text/javascript">
+    jQuery(function(){
+        //        jQuery( ".active" ).find('a').css('color','black');
+    });
+</script>
+
+<div class="work_enviro_header">
+
+</div>
+<div id="work_enviro_main" class="work_enviro_main">
+    <div class="work_enviro_main_content">
+        <div class="work_enviro_main_content_left">
+            <div class="work_enviro_main_content_title">环境未来</div>
+            <div style="">国际高校合作基于低碳设计的</div>
+            <div style="">工业废弃余料升级再利用</div>
+        </div>
+        <div class="work_enviro_main_content_right">
+            <p>2013年5月10日--5月24日设计长沙在中联重科的大力支持下举办了《设计长沙--环境未来 国际高校合作基于低碳设计的工业废弃余料升级再利用》
+                竞赛活动，这是一场由英国大使馆、湖南大学等企业和机构联合主办的国际竞赛项目。来自清华大学、湖南大学、广州美术学院、同济大学
+                、英国布鲁内尔大学、林肯大学、中央兰开夏大学、伦敦城市大学、韩国弘益大学等国际高校的60多名师生汇聚长沙，展开了14天的研讨，制作
+                过程。获奖作品在湖南省科技厅展出。</p>
+        </div>
+    </div>
+    <div style="clear:both"></div>
+</div>
+<div class="work_enviro_main_imgs">
+    <p>活动为期2周，发动国际高校等院校师生联合参与，在中国产业集聚区以工作坊的形式展开活动，以地毯设计竞赛贯穿，以工业废弃物为主要材料
+        进行创意设计，在现场制作完成作品食物、举办参赛作品公开展览，推动工业设计的低碳化和资源的再生利用。本活动还将通过国研讨会、校企互动、
+        媒体跟踪报道、中国获奖学生赴应交流考察等一系列重要环节的组织与实施，大力推动低碳思想与生态设计的传播与创新，促进设计成果的商业化，
+        把地毯设计理念转化为社会广泛接受的文明价值与经济价值。</p>
+
+    <img class="work_enviro_main_img" src="<?php print $theme_path ?>/images/work/enviro/1.jpg"
+    <img class="work_enviro_main_img" src="<?php print $theme_path ?>/images/work/enviro/2.jpg" />
+    <img class="work_enviro_main_img" src="<?php print $theme_path ?>/images/work/enviro/3.jpg" />
+    <img class="work_enviro_main_img" src="<?php print $theme_path ?>/images/work/enviro/4.jpg" />
+    <img class="work_enviro_main_img" src="<?php print $theme_path ?>/images/work/enviro/5.jpg" />
+    <img class="work_enviro_main_img" src="<?php print $theme_path ?>/images/work/enviro/6.jpg" />
+    <img class="work_enviro_main_img" src="<?php print $theme_path ?>/images/work/enviro/7.jpg" />
+</div>
+
