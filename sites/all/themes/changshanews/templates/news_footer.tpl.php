817a9bb15c22a5c0b224c2d9a2f2f34992afe704
Tom tang <tom.tang@perficientgdc.com.cn>
2014-02-08 17:08:00 +0800

    edit dashbard

--- sites/all/themes/changshanews/templates/news_footer.tpl.php
+++ sites/all/themes/changshanews/templates/news_footer.tpl.php
@@ -4,24 +4,26 @@
 
 <div class="news_footer">
     <div class="news_footer_contact">
-        <span>联系方式</span>
-        <span>地址：中国 长沙市绍山南路258号</span>
-        <span>邮编：410004</span>
-        <span>电话：0731-85019672 85015669</span>
-        <span>邮箱：fm-bo@foxmail.com</span>
+        <div style="float:left; display:inline-block;"><span class="footer_item">联系方式</span></div>
+        <div  style="float:left; display:inline-block;padding-left: 7px;">
+            <div style="float:left;">地址：中国 长沙市韶山南路258号</div>
+            <div style="float:left;">邮编：410004</div>
+            <div style="float:left;">电话：0731-85019672 85015669</div>
+            <div style="clear:left;">邮箱：fm-bo@foxmail.com</div>
+        </div>
     </div>
     <div class="news_footer_partner">
         <div>中南出版传媒集团</div>
         <div>潇湘晨报传媒经营有限公司</div>
-        <div>潇湘晨报传品牌创意公司</div>
+        <div>潇湘晨报品牌创意公司</div>
     </div>
     <div class="friend_link">
-        <span>友情链接</span>
-        <span><a href=""><img class="friend_link_img" src="<?php print $base_path.drupal_get_path('theme', 'changshanews'); ?>/images/footer/group01.png" alt=""></a></span>
-        <span><a href=""><img class="friend_link_img" src="<?php print $base_path.drupal_get_path('theme', 'changshanews'); ?>/images/footer/group02.png" alt=""></a></span>
+        <span class="footer_item">友情链接</span>
+        <span><a href=""><img class="friend_link_img" style=" width: 50px;height: 29px;" src="<?php print $base_path.drupal_get_path('theme', 'changshanews'); ?>/images/footer/group01.png" alt=""></a></span>
+        <span><a href=""><img class="friend_link_img" style=" width: 69px;height: 29px;" src="<?php print $base_path.drupal_get_path('theme', 'changshanews'); ?>/images/footer/group02.png" alt=""></a></span>
     </div>
     <div class="new_media">
-        <span>新媒体</span>
+        <span class="footer_item">新媒体</span>
         <span><a href=""><img class="new_media_img" src="<?php print $base_path.drupal_get_path('theme', 'changshanews'); ?>/images/footer/wb.png" alt=""></a></span>
         <span><a href=""><img class="new_media_img" src="<?php print $base_path.drupal_get_path('theme', 'changshanews'); ?>/images/footer/wx.png" alt=""></a></span>
         <span> <a href=""><img class="new_media_img" src="<?php print $base_path.drupal_get_path('theme', 'changshanews'); ?>/images/footer/facebook.png" alt=""></a></span>
