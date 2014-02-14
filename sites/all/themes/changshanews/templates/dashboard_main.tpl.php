8a6878983fe4a7f8f7a50745444d0096e470ee00
Tom tang <tom.tang@perficientgdc.com.cn>
2014-02-14 11:18:51 +0800

    change css

--- sites/all/themes/changshanews/templates/dashboard_main.tpl.php
+++ sites/all/themes/changshanews/templates/dashboard_main.tpl.php
@@ -30,9 +30,9 @@
 
 <div class="dashboard_main">
     <div class="dashboard_img dashboard_img1" id="dashboard_img1">
-        <div class="story">
-            <a href="" style="z-index: 999;padding-left: 40px;">
+         <a href="" style="z-index: 999;padding-left: 40px;">
             <img class="dashboard_main_logo small_log" src="<?php print drupal_get_path('theme', 'changshanews'); ?>/images/header/design_changsha_logo.png" alt=""/></a>
+        <div class="story">
             <!--        <a href="#" class="small_log"><img class=" small_log1" src="<?php //print drupal_get_path('module', 'dashboard'); ?>/images/main_image/dashboard_logo1.png"  alt=""/></a>-->
         </div>
     </div>
