82a9ba42dde42b56b39e4825986e2a715d857593
Tom tang <tom.tang@perficientgdc.com.cn>
2014-02-26 11:11:59 +0800

    change css

--- sites/all/themes/changshanews/templates/page--dashboard.tpl.php
+++ sites/all/themes/changshanews/templates/page--dashboard.tpl.php
@@ -4,7 +4,7 @@
         <div class="page-main" >
             <?php require_once 'first_header.tpl.php'; ?>
             <?php require_once 'second_header.tpl.php'; ?>
-            <?php //require_once 'dashboard_main.tpl.php'; ?>
+            <?php require_once 'dashboard_main.tpl.php'; ?>
         </div>
         <div id="dashboard_footer" class="dashboard_footer">
             <?php require_once 'news_footer.tpl.php'; ?>
